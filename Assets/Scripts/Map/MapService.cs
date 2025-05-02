b5275eef960e7f1ec9c073455a083383f5d21244
rangamach <117797904+rangamach@users.noreply.github.com>
2025-05-02 14:09:13 +0530

    made playerservice, mapservice, uiservice, waveservice, and soundservice as singletons

--- Assets/Scripts/Map/MapService.cs
+++ Assets/Scripts/Map/MapService.cs
@@ -16,6 +16,17 @@ namespace ServiceLocator.Map
         private MapData currentMapData;
         private SpriteRenderer tileOverlay;
 
+        public static MapService Instance { get { return instance; } }
+        private static MapService instance;
+
+        private void Awake()
+        {
+            if (instance == null)
+                instance = this;
+            else
+                Destroy(this.gameObject);
+        }
+
         private void Start()
         {
             SubscribeToEvents();
--- Assets/Scripts/Sound/SoundService.cs
+++ Assets/Scripts/Sound/SoundService.cs
@@ -9,6 +9,17 @@ namespace ServiceLocator.Sound
         [SerializeField] private AudioSource audioEffects;
         [SerializeField] private AudioSource backgroundMusic;
 
+        private static SoundService instance;
+        public static SoundService Instance { get { return instance; } }
+
+        private void Awake()
+        {
+            if (instance == null)
+                instance = this;
+            else
+                Destroy(this.gameObject);
+        }
+
         private void Start()
         {
             PlaybackgroundMusic(SoundType.BackgroundMusic, true);
