c00c3b51d1db0fac094f0f545f37de954611856f
rangamach <117797904+rangamach@users.noreply.github.com>
2025-05-02 14:24:39 +0530

    Added GenericMonoSingleton<T> class

--- Assets/Scripts/Map/MapService.cs
+++ Assets/Scripts/Map/MapService.cs
@@ -6,7 +6,7 @@ using ServiceLocator.Events;
 
 namespace ServiceLocator.Map
 {
-    public class MapService : MonoBehaviour
+    public class MapService : GenericMonoSingleton<MapService>
     {
         [SerializeField] private EventService eventService;
         [SerializeField] private MapScriptableObject mapScriptableObject;
@@ -15,17 +15,6 @@ namespace ServiceLocator.Map
         private Tilemap currentTileMap;
         private MapData currentMapData;
         private SpriteRenderer tileOverlay;
-
-        public static MapService Instance { get { return instance; } }
-        private static MapService instance;
-
-        private void Awake()
-        {
-            if (instance == null)
-                instance = this;
-            else
-                Destroy(this.gameObject);
-        }
 
         private void Start()
         {
--- Assets/Scripts/Wave/WaveService.cs
+++ Assets/Scripts/Wave/WaveService.cs
@@ -10,7 +10,7 @@ using ServiceLocator.Player;
 
 namespace ServiceLocator.Wave
 {
-    public class WaveService : MonoBehaviour
+    public class WaveService : GenericMonoSingleton<WaveService>
     {
         [SerializeField] private EventService eventService;
 
@@ -20,17 +20,6 @@ namespace ServiceLocator.Wave
         private int currentWaveId;
         private List<WaveData> waveDatas;
         private List<BloonController> activeBloons;
-
-        private static WaveService instance;
-        public static WaveService Instance { get { return instance; } }
-
-        private void Awake()
-        {
-            if (instance == null)
-                instance = this;
-            else
-                Destroy(this.gameObject);
-        }
 
         private void Start()
         {
